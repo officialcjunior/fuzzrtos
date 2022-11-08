35eece451e4fe91b2d5b99d04f8648a8f7ddda92
Aswin C <realc@protonmail.com>
2022-11-08 18:27:36 +0530

    Fix the name of the file in build.sh

--- protobufjsonharness/build.sh
+++ protobufjsonharness/build.sh
@@ -7,17 +7,20 @@ protoc --cpp_out=. json.proto
 protoc --cpp_out=. core_json.proto
 
 echo "[*] Building object files"
-clang++ json_writer.cpp -g -c
-clang++ json.pb.cc -g -c
-clang++ core_json_writer.cpp -g -c
-clang++ core_json.pb.cc -g -c
+clang++ -I /usr/include/google/protobuf -I /usr/local/include/libprotobuf-mutator json_writer.cpp -g -c
+clang++ -I /usr/include/google/protobuf -I /usr/local/include/libprotobuf-mutator json.pb.cc -g -c
+clang++ -I /usr/include/google/protobuf -I /usr/local/include/libprotobuf-mutator harness_corejson_parse.cpp -g -c
+clang++ -I /usr/include/google/protobuf -I /usr/local/include/libprotobuf-mutator  core_json.pb.cc -g -c
 clang++ -c /src/libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.cc \
         -I /usr/local/include/libprotobuf-mutator
+        -I /usr/include/google/protobuf
 clang++ -c /src/libprotobuf-mutator/src/libfuzzer/libfuzzer_mutator.cc \
         -I /usr/local/include/libprotobuf-mutator
+        -I /usr/include/google/protobuf
 clang++ -c harness_jq_pair.cpp \
         -g -fsanitize=address \
         -I /usr/local/include/libprotobuf-mutator
+        -I /usr/include/google/protobuf
 
 echo "[*] Linking executables"
 clang++ harness_jq_pair.o json_writer.o json.pb.o core_json_writer.o core_json.pb.o \
