29e64e6b520e249b3654b33498e00290f6c086d7
Aswin C <realc@protonmail.com>
2023-02-10 12:20:12 +0530

    Get rid of JSON stuff and fuzz StreamBuffer

--- protobuf-kernel/harness.cc
+++ protobuf-kernel/harness.cc
@@ -4,12 +4,41 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <string.h>
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "stream_buffer.h"
 
 // Fuzzer entrypoint
 extern "C" int fuzz(char *data, int mode_len, size_t size) {
 
-    int a = data[0];
-    
 
+    static StreamBufferHandle_t buffer;
+
+    // Avoid crashes on nulls
+    if(size < 2 || mode_len >= (size + 1)) {
+        return 0;
+    }
+
+    switch(mode_len) {
+
+        // Only validate
+        case 0:
+            //buffer = data;
+           // bufferLength = sizeof(buffer) - 1;
+            
+           // xQueueGenericCreate( mode_len, mode_len, size );
+            break;
+        
+        // Validate and search
+        default:
+
+
+            buffer = xStreamBufferCreate(100, 1);
+            xStreamBufferSend(buffer, data, mode_len, portMAX_DELAY);
+            xStreamBufferReceive(buffer, data, mode_len, portMAX_DELAY);            
+            free(buffer);
+        break;       
+
+    }
     return 0;
 }
