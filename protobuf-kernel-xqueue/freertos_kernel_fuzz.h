e2f2f136d0f3d98f3f7f8b9e052f556f92cc55a4
fug1t1ve <gbajeli36@gmail.com>
2023-02-17 01:55:41 +0530

    Have to fix xqueue

---
+++ protobuf-kernel-xqueue/freertos_kernel_fuzz.h
@@ -0,0 +1,5 @@
+#include "rtos_protocol_buffers.pb.h"
+#include <bits/stdc++.h>
+#include <string>
+
+std::string SerializeStreambuffer(const StreamBuffer &test_proto);
---
+++ protobuf-kernel-xqueue/harness.cc
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "queue.h"
+
+// Fuzzer entrypoint
+extern "C" int fuzz(char *data, int mode_len, size_t size) {
+
+    QueueHandle_t xQueue;
+
+    // Avoid crashes on nulls
+    if(size < 2 || mode_len >= (size + 1)) {
+        return 0;
+    }
+
+    switch(mode_len) {
+
+        // Only validate
+        case 0:
+            //buffer = data;
+           // bufferLength = sizeof(buffer) - 1;
+            
+           // xQueueGenericCreate( mode_len, mode_len, size );
+            break;
+        
+        // Validate and search
+        default:
+            xQueue = xQueueCreate(1, sizeof( char * ));
+            xQueueSend(xQueue, ( void * ) data, ( TickType_t ) 0 );
+            xQueueReceive(xQueue, ( void * ) data, 0);            
+            free(xQueue);
+        break;       
+
+    }
+    return 0;
+}
